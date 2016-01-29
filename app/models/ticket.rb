4eb886bbf3bc7f08237cb0147fcdc3e90129c39b
Devan Kestel <devan.kestel@gmail.com>
2016-01-29 00:16:22 -0500

    Added dependent destroy to statuses on tickets.
    Statuses are meaningless if the ticket is deleted.

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,5 @@
 class Ticket < ActiveRecord::Base
-  has_many :statuses
+  has_many :statuses, depdendent: :destroy
   belongs_to :user
   belongs_to :assignee, class_name: 'User',
               foreign_key: 'assignee_id'
